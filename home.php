a6de7481f32ff6152d1a81e75cf15c642fd52acc
Your Name <you@example.com>
2021-01-25 01:07:48 -0800

    upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/logo-ubg.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Komunitas Desain Grafis</font> <br>
+<font Size="6">Universitas Bumigora Mataram</font>
+</center>
